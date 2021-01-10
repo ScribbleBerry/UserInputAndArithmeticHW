af16603bd6cf351357d2fe45a1ff2a325ed5affe
ScribbleBerry <72979403+ScribbleBerry@users.noreply.github.com>
2021-01-10 08:28:39 +0530

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,41 @@
+L= (‘enter a number’)
+B= (‘enter a number’)
+Area= L*B 
+print = ("area= " area)
+
+Pen_cost=("enter the number of pens")*20
+Pencil_cost=("enter the number of pens")*3
+Print("cost of pens = " Pen_cost’)
+Print("cost of pencils =" Pencil_cost)
+
+A = ("enter 1st students score")
+B = ("enter 2nd students score")
+C = ("enter 3rd students score")
+Average= A+B+C / 3
+
+Tomato= (‘enter tomato quantity’)*40
+bean= (‘enter bean quantity’)*50
+print(‘price of tomato is :Tomato’)
+print(‘price of bean is :Bean’)
+
+Sub_1=(‘enter marks’)
+Sub_2=(‘enter marks’)
+Sub_3=(‘enter marks’)
+Sub_4=(‘enter marks’)
+Sub_5=(‘enter marks’)
+Sub_6=(‘enter marks’)
+
+Amount = int(input(‘enter the amount’)
+Time = int(input(‘enter time’)
+Final_amt = amount*time*9 / 100
+Print (‘the final amount is’ = final_amt)
+
+Quantity = int(input(‘enter quantity’)
+Rate= int(input(‘enter rate’)
+Bill = quantity*rate
+Print =(‘Your bill is’= bill)
+
+
+
+
+
---
+++ UserInputAndDataTypes.py
@@ -0,0 +1,23 @@
+#Example un
+
+print(37 / (5 * 2))
+print(37 / 5 / 2)
+print(37 * (5 / 2))
+print(37 % (5 % 2))
+print(37 % 5 % 2)
+print(37 - 5 * 2)
+
+#Example deux
+
+a = 15
+b = 5
+c = 2
+
+print(a / b *c)
+print((a + b) * c)
+print((a % b) * c)
+print((a + b) % c)
+print(a / b - c)
+print(a + b - c)
+
+
